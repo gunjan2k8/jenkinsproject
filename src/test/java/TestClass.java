c4fc655c03472ea8c380fd54fd6ab83a730cbfb7
gunjan2k8 <gunjan2k8@gmail.com>
2022-01-24 17:51:50 +0530

    jenkins project

---
+++ src/test/java/TestClass.java
@@ -0,0 +1,20 @@
+import org.testng.annotations.Test;
+
+public class TestClass {
+
+    @Test
+    public void test1(){
+        System.out.println("test1");
+
+    }
+
+
+    @Test
+    public void test2(){
+        System.out.println("test2");
+
+    }
+
+
+
+}
